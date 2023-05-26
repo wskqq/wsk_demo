1bcc390706a5df2cdfb8caeee9d28be273de0ffc
wsk <wsk@wsk.com>
2023-05-26 17:30:29 +0800

    新增netty的demo注释

--- wsk_demo/src/main/java/example/netty/server/HttpServerChannelHandler.java
+++ wsk_demo/src/main/java/example/netty/server/HttpServerChannelHandler.java
@@ -23,7 +23,12 @@ public class HttpServerChannelHandler extends SimpleChannelInboundHandler<String
         System.out.println("接收到客户端数据[" + s + "]");
         String responseMsg = "Hello World";
 
-        // TODO 两种返回数据的方式与InboundHandler与outboundHandler添加顺序有关
+        // TODO 两种返回数据的方式与InboundHandler与outboundHandler添加顺序有关，
+        //  顺序与调用方式不匹配会导致，Handler不执行
+        // TODO Ctx.write()：仅仅是将我们的信息加入到队列里面，并没有发生出去。是在当前handler寻找下一个handler，并不是将这个pipline重新走了一遍
+        //  Ctx.writeAndFlush()：是向前找到第一个遇到的OutHandler，再发送出去，也并不是将这个pipline重新走了一遍。
+        //  Ctx.channel.writeAndFlush()：这个是将这个pipline重新走了一遍，可能会引起死循环，假如这个handler是中间的handler，
+        //  他会将这个pipline重新走了一遍，等走到原点，又会有机会执行Ctx.channel.writeAndFlush()，这个大部分是用在客户端。
         ctx.writeAndFlush(responseMsg);
 //        ctx.channel().writeAndFlush(responseMsg);
 
--- wsk_demo/src/main/java/example/netty/server/HttpServerInitializer.java
+++ wsk_demo/src/main/java/example/netty/server/HttpServerInitializer.java
@@ -25,6 +25,9 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
     /**
      * ChannelInboundHandlerAdapter 实现类执行顺序与添加顺序一致
      * ChannelOutboundHandlerAdapter 实现类执行顺序与添加顺序相反
+     * pipeline.addLast 从tail端添加处理器
+     * pipeline.addFirst 从head端添加处理器
+     * ctx.fireChannelRead() 作用是将数据从处理器传递到下一个处理器，没有这个，后面的处理器不会调用
      * @param socketChannel
      * @throws Exception
      */
@@ -32,13 +35,14 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
 
-        //http请求编解码器,请求解码，响应编码 ChannelInboundHandlerAdapter
-//        pipeline.addLast( new HttpServerCodec());
-//
+        // TODO 目前添加该处理器之后，其他处理器不执行，应该是ctx.fireChannelRead()未执行，原因待分析？？？
+        //  http请求编解码器,请求解码，响应编码 ChannelInboundHandlerAdapter,只能处理get请求，其他请求执行该处理器之后无法继续执行
+//        pipeline.addLast(new HttpServerCodec());
+
         //http请求报文聚合为完整报文，最大请求报文为10M ChannelInboundHandlerAdapter
-//        pipeline.addLast(new HttpObjectAggregator(10 * 1024 * 1024));
+        pipeline.addLast(new HttpObjectAggregator(10 * 1024 * 1024));
         //响应报文压缩  ChannelInboundHandlerAdapter
-//        pipeline.addLast("compress", new HttpContentCompressor());
+        pipeline.addLast(new HttpContentCompressor());
         //业务处理handler  ChannelInboundHandlerAdapter
         pipeline.addLast(new StringDecoder());
 
@@ -49,9 +53,6 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast(new StringEncoder());
         // TODO ChannelInboundHandlerAdapter
         pipeline.addLast(new HttpServerChannelHandler());
-        // TODO 添加多个处理器时，服务端就无法接收到客户端请求，原因待分析？？？？？？
-        // 原因可能是请求数据被处理器拦截
-//        pipeline.addLast(new ChannelHandler[]{new HttpServerCodec()});
 
         // ChannelOutboundHandlerAdapter
         // TODO 添加方式顺序二：
